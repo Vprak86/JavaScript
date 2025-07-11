cf2782a718fcdab6c0f61d64b5593954001a6265
vprak86 <prak2003v@gmail.com>
2025-07-11 11:13:41 +0530

    initial

---
+++ basic_01.js
@@ -0,0 +1,21 @@
+// const StudentId = 134;
+// let studentName = "prakhar";
+// var collageName = "NIET";
+
+// console.log(StudentId);
+// console.log(studentName);
+// console.log(collageName);
+
+// console.table([StudentId, studentName,collageName]);
+
+
+//var donot have scope feature;
+//null=>standalone value;   //type =>object
+//undefined =>datatype;    //type => undefined {let state;} typeof(state)= undefined
+
+// console.log(2+"3");   //23
+// console.log("2"+3);   //23
+
+// console.log(1+2+"3");   //33;
+
+// console.log("1"+2+3);   //123

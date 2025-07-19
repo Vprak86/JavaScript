b1256362e4694785f802b6c30b53540a86befccb
vprak86 <prak2003v@gmail.com>
2025-07-19 10:59:59 +0530

    BMI calculator

--- Project/BMI Calculator/script.js
+++ Project/BMI Calculator/script.js
@@ -5,6 +5,12 @@
 
 const form = document.querySelector("form")
 
+const div = document.getElementById("result");
+
+  if (!div.textContent.trim()) {
+    div.style.display = "none";
+  }
+
 form.addEventListener('submit',function(e){
     e.preventDefault();
 
@@ -12,11 +18,34 @@ form.addEventListener('submit',function(e){
     let weight = parseInt(document.querySelector('#weight').value)
 
     let result = document.querySelector('#result');
+    div.style.display = "";
 
-    const ans = weight/(height*height);
-    console.log(ans);
+    if(weight ==='' || weight < 0 || isNaN(weight)){
+        result.innerHTML = `Please give a valid weight ${weight}`
+    }
+    else if(height ==='' || height < 0 || isNaN(height)){
+        result.innerHTML = `Please give a valid height ${height}`
+    }
+    else{
+        const ans = (weight/((height*height)/10000)).toFixed(2);
+        console.log(ans);
 
-    result.innerHTML = ans;
+        result.innerHTML = ans;
+        if(ans < 18.6){
+            
+            result.innerHTML = `${ans} <br> Underweight`;
+        }
+        else if(ans >= 18.6 && ans <= 24.6){
+            
+            result.innerHTML = `${ans} <br> Normal`;
+        }else{
+            
+            result.innerHTML = `${ans} <br> Overweight`;
+        }
+    }
+    
+    
 
+    
     
 })
